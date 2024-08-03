836e047d0d0fe3ba5ddfdd17ed5c2678daaf7101
Aleksandar Micic <AMicic1997@gmail.com>
2024-08-03 11:52:03 +0200

    Project setup

---
+++ GreekKino/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GreekKino
+//
+//  Created by Aleksandar Micic on 3.8.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GreekKino/GreekKinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreekKinoApp.swift
+//  GreekKino
+//
+//  Created by Aleksandar Micic on 3.8.24..
+//
+
+import SwiftUI
+
+@main
+struct GreekKinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
